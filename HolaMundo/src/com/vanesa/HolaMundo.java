01e3978f3dad8b34383db5f32a3f60d589cd7471
usuario <usuario@localhost>
2018-04-16 22:22:21 +0000

    añadimos mensaje

--- HolaMundo/src/com/vanesa/HolaMundo.java
+++ HolaMundo/src/com/vanesa/HolaMundo.java
@@ -3,7 +3,7 @@ package com.vanesa;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 		
 		System.out.println("Hola Mundo");
 		System.out.println("Adios");
